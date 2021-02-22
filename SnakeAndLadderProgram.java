071609d05999d99bf8f9bee2e33c0576d97d745c
RachanaBR <brrachanajain@gmail.com>
2021-02-22 21:11:57 +0530

    Player rolls the die to get number b/w 1-6 using Random()

--- SnakeAndLadderProgram.java
+++ SnakeAndLadderProgram.java
@@ -4,4 +4,15 @@ starts from 0 rolls the die to get a number between 1 to 6, finds
 a safe place, ladder or a snake keeps doing till the winning spot
 
 100 is achieved.*/
+public class SnakeAndLadderProgram {
+	
+	public static void main(String[] args)
+	{
+		//using Random to die count b/w 1-6
+		 int  diceRoll = (int)(Math.random()*6 )+1;
+		 int pos=0;
+		 System.out.println("position of player is in ="+ pos+ "rolls die "+diceRoll);	    
+	}
+
+}
 
