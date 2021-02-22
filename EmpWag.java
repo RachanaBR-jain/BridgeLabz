7b9a9b854ad3ba30538570ea4e03fd1337ae2daf
RachanaBR <brrachanajain@gmail.com>
2021-02-22 13:00:29 +0530

    added total emphrs for 100hrs and 20 days

--- EmpWag.java
+++ EmpWag.java
@@ -1,24 +1,26 @@
-public class EmpWag
- {
+public class EmpWag {
 	public static final int IS_FULL_TIME=1;
 	public static final int IS_PART_TIME=2;
-	public static final int NUM_OF_WORKING_DAYS=2;
+	public static final int NUM_OF_WORKING_DAYS=20;
 	public static final int WAGE_RATE=20;
-	
+	public static final int MAX_HRS_IN_MONTH=100;
 	public static void main(String[] args) {
 		
 		int empHr=0;
-		int salary=0;
-		int salaryForMonth=0;
-	
+		int totalWorkingDays=0;
+		int totalEmpHrs=0;
+
 		
-		//Computation
-		for(int day=0;day<NUM_OF_WORKING_DAYS;day++)
+		while(totalEmpHrs<=MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
 		{
+			
+			totalWorkingDays++;
 			int empCheck=(int)Math.floor(Math.random()*10)%3;
+			
 			switch(empCheck) {
 		
-				case IS_FULL_TIME:System.out.println("employee is Present");
+				case IS_FULL_TIME:	
+							System.out.println("employee is Present");
 							empHr=8;
 							break;
 				case IS_PART_TIME:
@@ -29,14 +31,17 @@ public class EmpWag
 						System.out.println("employee is Absent");
 						empHr=0;	
 			}
-	
-			salary=WAGE_RATE*empHr;
-			salaryForMonth+=salary;
-			System.out.println("employee salary is ="+salary);
+			
+			totalEmpHrs+=empHr;
+			
+			System.out.println("days="+totalWorkingDays+" emphour="+empHr);
 		}
-		System.out.println("employee salary for a Month is ="+salaryForMonth);	 	
+			int salaryForMonth=totalEmpHrs * WAGE_RATE;
+		    System.out.println("employee salary for a Month is ="+salaryForMonth); 	
 	}
 }
+
+
 
 
 
